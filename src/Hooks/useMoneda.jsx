7d8e941c5c2a05b3008c7867400ac7da01e4e4de
Osiris Román <osirisr1994@gmail.com>
2020-12-06 18:11:38 -0500

    Styles components dentro del hook personalizado.

--- src/Hooks/useMoneda.jsx
+++ src/Hooks/useMoneda.jsx
@@ -1,20 +1,22 @@
 import React, { Fragment, useState } from 'react';
 
+import { Label, Select } from './useMoneda.styles';
+
 const useMoneda = (label, stateInicial, opciones) => {
   //Estado de nuestro Hook
   const [state, setState] = useState(stateInicial);
 
   const Seleccionar = () => (
     <Fragment>
-      <label>{label}</label>
-      <select>
+      <Label>{label}</Label>
+      <Select onChange={(e) => setState(e.target.value)} value={state}>
         <option value=''>- Seleccione -</option>
         {opciones.map((opcion) => (
           <option key={opcion.codigo} value={opcion.codigo}>
             {opcion.nombre}
           </option>
         ))}
-      </select>
+      </Select>
     </Fragment>
   );
 
---
+++ src/Hooks/useMoneda.styles.jsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const Label = styled.label`
+  font-family: 'Bebas Neue', cursive;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 2.4rem;
+  margin-top: 2rem;
+  display: block;
+`;
+
+export const Select = styled.select`
+  width: 100%;
+  display: block;
+  padding: 1rem;
+  -webkit-appearance: none;
+  border-radius: 10px;
+  border: none;
+  background: url(http://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/br_down.png)
+    no-repeat right #ddd;
+  background-position-x: 98%;
+  font-size: 1.2rem;
+`;
